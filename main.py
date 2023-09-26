088b894f60aad2426bce72f0720b7d301b16f075
leonardo ramos <leeeoramos@gmail.com>
2023-09-26 12:08:17 -0300

    :bug: fix: Ajuste na função do ChromeDriver

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
-
-def open_site():
+def brownser(url):
     driver = webdriver.Chrome()
 
-    driver.get("https://gizmodo.uol.com.br/")
+    driver.get(url)
 
+    title = driver.title
+    assert title == "Web form"
 
-open_site()
+    driver.implicitly_wait(0.5)
+
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+    text_box.send_keys("Selenium")
+    submit_button.click()
+
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    assert value == "Received!"
+    time.sleep(40)
+
+    # driver.quit()
+
+if __name__ == "__main__":
+    url = 'https://www.selenium.dev/selenium/web/web-form.html'
+    brownser(url)
