1c4cf8e60fc2b0f72e7fde108818ddf78ad20f37
autor <leeeoramos@gmail.com>
2023-09-30 22:47:49 -0300

    feat: get element content

--- main.py
+++ main.py
@@ -3,24 +3,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 driver = webdriver.Chrome()
 
 driver.get('https://gizmodo.uol.com.br/')
 
-post_elements = driver.find_elements(By.CLASS_NAME, 'postHeader')
+post_elements = driver.find_elements(By.CLASS_NAME, 'list-item')
 
 titles = []
 dates = []
 summaries = []
 
-for post_element in post_elements[:10]:
-    # title_element = post_element.find_element(By.CLASS_NAME, 'postTitle')
-    date_element = post_element.find_element(By.CLASS_NAME, 'published updated')
-    # summary_element = post_element.find_element(By.CLASS_NAME, 'postSummary entry-content')
-    # titles.append(title_element.text)
-    dates.append(date_element)
-    # summaries.append(summary_element)
 
-for _ in range(10):
-    print('Title, Date, Resumo.')
-    print(titles[_],dates[_],summaries[_])
+
+for post_element in post_elements[:10]:
+    title_element = post_element.find_element(By.CLASS_NAME, 'postHeader')
+    date_element = post_element.find_element(By.CSS_SELECTOR, 'span.metaText.metaDate')
+    summary_element = post_element.find_element(By.CLASS_NAME, 'postSummary')
+    date_text = driver.execute_script("return arguments[0].textContent;", date_element)
+    summary_text = driver.execute_script("return arguments[0].textContent;", summary_element)
+    titles.append(title_element.text)
+    dates.append(date_text.strip())
+    summaries.append(summary_text.strip())
+
